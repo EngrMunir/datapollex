f3af853c396505718da4c72928fe4acaea715696
munir <smsirajulmonir@gmail.com>
2025-07-01 15:58:14 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json())
 
 app.use(
     cors({
-         origin: ['http://localhost:3000'],
+         origin: ['http://localhost:3000', 'https://lms-frontend-azure-tau.vercel.app'],
         credentials: true 
     }))
 
